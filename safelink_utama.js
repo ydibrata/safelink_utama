7b137c089843b62664ce29c71ad9952a6db06632
NAZAR YUDA DIBRATA <bontotspeed.yuda@gmail.com>
2018-06-22 00:12:06 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="brand-ambassador-staffing-for.html"}
 if(x=="2"){xxx="the-future-of-gardiner-expressway.html"}
 if(x=="3"){xxx="why-custom-web-development-here-is.html"}
-if(x=="4"){xxx="promoting-your-content-effectively-so.html"}a_to_ve[i].href="https://www.berpusat.id/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="promoting-your-content-effectively-so.html"}
+if(x=="5"){xxx="type-2-diabetes-obesity-and-non.html"}
+if(x=="6"){xxx="5-emerging-tech-trends-for-career.html"}a_to_ve[i].href="https://www.berpusat.id/2018/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
